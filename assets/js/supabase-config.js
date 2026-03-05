299ff87e61fdad53557ae1986736a3862aa33bf0
Vip <professional.inbox.simo@gmail.com>
2026-03-05 07:15:04 +0000

    Add files via upload

---
+++ assets/js/supabase-config.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = 'https://hmlqppacanpxmrfdlkec.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_Z5g0nS3Bv2WH8Z9gAk-1Jw_TJdurs9y';
+window.supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
